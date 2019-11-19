5cbb976942a580fe7d1785fd8366ca87bea8a336
Matt Hinojosa <mhinojosa0022@hotmail.com>
2019-11-19 16:41:35 -0600

    LoginPage
    Working LoginPage with INavService for both iOS and Android

---
+++ BarDemo/Views/LoginPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using BarDemo.Models;
+using BarDemo.ViewModels;
+using BarDemo.Services;
+using System.Diagnostics;
+using Xamarin.Auth;
+using System.Linq;
+using Newtonsoft.Json;
+using Xamarin.Essentials;
+
+
+
+namespace BarDemo.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+
+        LoginViewModel _vm
+        {
+            get { return BindingContext as LoginViewModel; }
+        }
+
+        Account account;
+        // AccountStore store;
+
+
+        public LoginPage()
+        {
+            InitializeComponent();
+            // store = AccountStore.Create();
+
+            //BindingContext = new LoginViewModel();
+            BindingContext = new LoginViewModel(DependencyService.Get<INavService>());
+
+            if (IsBusy)
+                return;
+            IsBusy = true;
+        }
+
+
+        void FBCommand(object sender, EventArgs e)
+        {
+            var item = sender;
+            _vm.FBCommand.Execute(item);
+        }
+
+
+
+    }
+}
+
+
